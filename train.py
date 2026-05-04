2381c37c191da15defd156025abdf6aa0746ea20
Qicen <1431244732@qq.com>
2026-05-04 16:19:17 +0800

    修改统一后的预训练参数

--- train.py
+++ train.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
   model = YOLO('ultralytics\\cfg\\models\\26\\yolo26-pose.yaml')
   # model.load('yolo11n.pt')  #注释则不加载
   results = model.train(
-    data='C:\\git_lib\\YOLO_test\\ultralytics\\data_8_yolo_lettuce\\pose_auto.yaml',  #数据集配置文件的路径
+    data=r'C:\git_lib\YOLO_test\ultralytics\data_3_yolo_mix\weed_lettuce.yaml',  #数据集配置文件的路径
     epochs=300,  #训练轮次总数
-    batch=8,  #批量大小，即单次输入多少图片训练
+    batch=4,  #批量大小，即单次输入多少图片训练
     imgsz=640,  #训练图像尺寸
     workers=0,  #加载数据的工作线程数
     device= 0,  #指定训练的计算设备，无nvidia显卡则改为 'cpu'
-    optimizer='Adam',  #训练使用优化器，可选 auto,SGD,Adam,AdamW 等
+    optimizer='AdamW',  #训练使用优化器，可选 auto,SGD,Adam,AdamW 等
     amp= True,  #True 或者 False, 解释为：自动混合精度(AMP) 训练
     cache=False,  # True 在内存中缓存数据集图像，服务器推荐开启
     # close_mosaic_border=False,  # True 删除边界的mosaic图像
